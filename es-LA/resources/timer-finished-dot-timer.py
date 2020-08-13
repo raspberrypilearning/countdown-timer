c06b06f6c18c5bc17087675d205724bb0f98f999
majamanojlovic <49232422+majamanojlovic@users.noreply.github.com>
2020-08-13 13:59:39 +0100

    New translations timer-finished-dot-timer.py (Spanish, Latin America)

---
+++ es-LA/resources/timer-finished-dot-timer.py
@@ -0,0 +1,27 @@
+#!/bin/python3
+
+from sense_hat import SenseHat
+from time import sleep
+
+sense = SenseHat()
+
+G = [0, 255, 0] # verde
+R = [255, 0, 0] # rojo
+X = [0, 0, 0]  # desactivado
+
+secs = 10
+timer = [ G for i in range(secs)] + [ X for j in range(64 - secs) ]
+
+sense.set_pixels(timer)
+
+#temporizador de 30 segundos
+for i in range(0, secs):
+  sleep(1)
+  timer[i] = R
+  sense.set_pixels(timer)
+
+for i in range(0, 10):
+  sense.clear()
+  sleep(0.1)
+  sense.set_pixels(timer)
+  sleep(0.1)
