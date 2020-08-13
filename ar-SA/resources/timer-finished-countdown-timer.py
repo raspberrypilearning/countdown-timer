31936992d3df0b9f3ad028fe1456606b14f73c1c
majamanojlovic <49232422+majamanojlovic@users.noreply.github.com>
2020-08-13 13:52:31 +0100

    New translations timer-finished-countdown-timer.py (Arabic)

--- ar-SA/resources/timer-finished-countdown-timer.py
+++ ar-SA/resources/timer-finished-countdown-timer.py
@@ -3,9 +3,9 @@
 from sense_hat import SenseHat
 from time import sleep
 
-sense = SenseHat()
+المتحسس= (SenseHat)
 
-secs = 5
+ثانية = 5
 
 for i in range(0,secs+1):
   sense.show_letter(str(secs-i), text_colour=[0, 255, 0])
