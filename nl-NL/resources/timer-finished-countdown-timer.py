4ba3317cda23604e5675f17b45d739c2f207c3a9
majamanojlovic <49232422+majamanojlovic@users.noreply.github.com>
2021-02-02 12:10:54 +0000

    New translations timer-finished-countdown-timer.py (Dutch)

--- nl-NL/resources/timer-finished-countdown-timer.py
+++ nl-NL/resources/timer-finished-countdown-timer.py
@@ -5,8 +5,8 @@ from time import sleep
 
 sense = SenseHat()
 
-secs = 5
+seconden = 5
 
-for i in range(0,secs+1):
-  sense.show_letter(str(secs-i), text_colour=[0, 255, 0])
+for i in range(0,seconden+1):
+  sense.show_letter(str(seconden-i), text_colour=[0, 255, 0])
   sleep(1)
